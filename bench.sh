a7a6cb17af3150761fda44740afd284c307a2f48
Maciej Czekaj <mjc@semihalf.com>
2014-12-11 17:45:53 +0100

    Improved scripts.

--- bench.sh
+++ bench.sh
@@ -1,6 +1,8 @@
+file=${1:=res.txt}
 for i in {0..20}
 	do
-	taskset 1 ./mem $(( (1024<<i) - 1)) | awk '{printf "%.1f %s\n", $1/1024, $2}'
-	taskset 1 ./mem $(( ( (1024<<i) * 5 - 1) / 4 )) | awk '{printf "%.1f %s\n", $1/1024, $2}'
-	taskset 1 ./mem $(( ( (1024<<i) * 7 - 1) / 4 )) | awk '{printf "%.1f %s\n", $1/1024, $2}'
-	done | tee res.txt
+	taskset 1 ./mem $(( (1024<<i) )) | awk '{printf "%.1f %s\n", $1/1024, $2}'
+	taskset 1 ./mem $(( ( (1024<<i) * 5) / 4 )) | awk '{printf "%.1f %s\n", $1/1024, $2}'
+	taskset 1 ./mem $(( ( (1024<<i) * 7) / 4 )) | awk '{printf "%.1f %s\n", $1/1024, $2}'
+	done | tee $file
+
--- l1.sh
+++ l1.sh
@@ -1,1 +1,1 @@
-perf stat -e cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-prefetch-misses,L1-dcache-loads,LLC-loads,LLC-prefetches,dTLB-loads,dTLB-load-misses taskset 1 $@
+perf stat -e cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-prefetch-misses,L1-dcache-loads,LLC-loads,LLC-prefetches,dTLB-loads,dTLB-load-misses,page-faults taskset 1 $@

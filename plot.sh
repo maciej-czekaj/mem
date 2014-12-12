ba1e401c941b72a2e0b714cde748690451361dc9
Maciej Czekaj <mjc@semihalf.com>
2014-12-12 13:04:14 +0100

    New plot for many files.

--- plot.sh
+++ plot.sh
@@ -1,2 +1,8 @@
-gnuplot -e "set logscale x 2; set logscale y 2; plot \"$1\"; pause -1"
+s=""
+for f in $*
+do
+	s+=\"$f\",
+done
+
+gnuplot -e "set logscale x 2; set logscale y 2; plot ${s%,}; pause -1"
 
--- plot2.sh
+++
@@ -1,8 +0,0 @@
-s=""
-for f in $*
-do
-	s+=\"$f\",
-done
-
-gnuplot -e "set logscale x 2; set logscale y 2; plot ${s%,}; pause -1"
-

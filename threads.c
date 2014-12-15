9adb617ae6fc2c1df4bbe08ba0c15ebeb5c1e632
Maciej Czekaj <mjc@semihalf.com>
2014-12-15 10:25:38 +0100

    More benchmarks

--- threads.c
+++ threads.c
@@ -20,52 +20,99 @@ uint64_t getclock()
 }
 
 #define N (500*1000*1000)
+#define MAXTHREADS 16
 
 static pthread_barrier_t barrier;
+static char *mem;
+static long (*benchmark)(long *);
 
-void benchmark(long *p)
+long benchmark_v(long *p)
 {
 	unsigned i;
 
 	for (i=0; i < N; i++) {
 		*(volatile long *)p += 1;
-		//asm volatile ("" :: "m" (*p));
 	}
+	return N;
+}
+
+long benchmark_s(long *p)
+{
+	unsigned i;
+	unsigned n = N/10;
+
+	for (i=0; i < n; i++) {
+		__sync_add_and_fetch(p, 10);
+	}
+	return n;
+}
+
+long benchmark_r(long *p)
+{
+	unsigned i;
+	long r;
+
+	for (i=0; i < N; i++) {
+		if (p == (long *)mem)
+			*(volatile long *)p += 1;
+		else
+			r = *(volatile long *)p;
+	}
+	(void)r;
+	return N;
 }
 
 static void *thread(void *arg)
 {
 	long *p = arg;
 	uint64_t t1, t2;
-
-//	printf("%p\n", p);
+	long n;
+	float *res;
 
 	pthread_barrier_wait(&barrier);	
 
 	t1 = getclock();
 
-	benchmark(p);
+	n = benchmark(p);
 	
 	t2=getclock();
 
-	return (void *)(t2 - t1);
+	res = malloc(sizeof(float));
+	*res = (t2 - t1)/(n*1.0);
+
+	return (void *)res;
 }
 
-#define MAXTHREADS 16
 
 int main(int argc, char **argv) 
 {
-	char *mem;
 	unsigned i;
 	unsigned nthreads = 2;
 	unsigned pad;
 	pthread_t threads[MAXTHREADS];
 	pthread_attr_t attr;
 	cpu_set_t c;
-	uint64_t time;
+	float *time;
 
-	if (argc < 2 || sscanf(argv[1], "%u", &pad) < 1)
+	if (argc < 3)
 		return 1;
+
+	if ( sscanf(argv[1], "%u", &pad) < 1)
+		return 1;
+
+	switch(argv[2][0]) {
+	
+	case 'v':
+		benchmark = benchmark_v;
+		break;
+	case 's':
+		benchmark = benchmark_s;
+		break;
+	case 'r':
+		benchmark = benchmark_r;
+		break;	default:
+		benchmark = NULL;
+	}
 
 	if (posix_memalign((void **)&mem, 64, nthreads*pad)) {
 		perror("posix_memalign");
@@ -88,16 +135,13 @@ int main(int argc, char **argv)
 	pthread_setaffinity_np(pthread_self(), sizeof(c), &c);
 	mem[0] = 0;
 
-	time = (uint64_t) thread(&mem[0]);
+	time = (float *) thread(&mem[0]);
 
-	printf("%.2f\n", (time*1.0)/N);
-
-	assert(*(long *)&mem[0] == N);
+	printf("%.2f\n", *time);
 
 	for (i = 1; i < nthreads; i++) {
 		pthread_join(threads[i], (void **)&time);
-		assert(*(long *)&mem[i*pad] == N);
-		printf("%.2f\n", (time*1.0)/N);
+		printf("%.2f\n", *time);
 	}
 		
 	return 0;

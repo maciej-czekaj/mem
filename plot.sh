7eeec8df88441d6f8f14792587bd3266f9c26348
Maciej Czekaj <maciej.jakub.czekaj@gmail.com>
2015-01-08 23:56:42 +0100

    Add hints to plot.sh

--- plot.sh
+++ plot.sh
@@ -4,5 +4,7 @@ do
 	s+="\"$f\" using (\$1*1024):(\$2) title '$f',"
 done
 
-gnuplot -e "set logscale x 2; set format x \"%.0b%B\"; set logscale y 2; plot ${s%,}; pause -1"
-
+cmd="set logscale x 2; set format x \"%.0b%B\"; set logscale y 2; plot ${s%,}; pause -1"
+echo $cmd
+gnuplot -e "$cmd"
+#set term png size 1024,768; set output '1.png'; load 'file'

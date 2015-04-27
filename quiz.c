5250a5be6b54a5f53c270e3d3dc9ac050b95a5d3
Maciej Czekaj <maciej.jakub.czekaj@gmail.com>
2015-04-27 23:15:13 +0200

    Added single step mode.

--- quiz.c
+++ quiz.c
@@ -17,7 +17,7 @@ uint64_t getclock()
 	return (uint64_t)ts.tv_sec * 1000000000 + ts.tv_nsec;
 }
 
-#define N ((8<<20)/sizeof(long))
+#define N ((8<<20)/sizeof(long)) //8MB
 #define K ((1<<30) / N)
 
 long a[N] __attribute__((aligned(64)));
@@ -25,15 +25,23 @@ long a[N] __attribute__((aligned(64)));
 void quiz(unsigned step)
 {
 	unsigned k,i;
-	for (k=0;k < K ;k++)
-		for (i=0; i< N; i+=step)
+	for (k = 0; k < K ; k++)
+		for (i = 0; i < N; i += step)
 			a[i] *= 3;
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
 	unsigned step;
 	memset(a,1,sizeof(a));
+	if (argc == 2) {
+		uint64_t time = getclock();
+		step = atoi(argv[1]);
+		quiz(step);
+		time = getclock() - time;
+		printf("%u %.1f\n", step, time/10e6);
+		return 0;
+	} 
 	for (step=1; step < 16; step++) {
 		uint64_t time = getclock();
 		quiz(step);

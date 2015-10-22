934c7b93f02818748e337f075811840187e0d29d
Maciej Czekaj <mjc@semihalf.com>
2015-10-22 15:11:11 +0200

    account for 128B cache line in some architectures.

--- threads.sh
+++ threads.sh
@@ -1,11 +1,11 @@
 ./l1.sh ./threads 16 v
-./l1.sh ./threads 64 v
+./l1.sh ./threads 128 v
 
 ./l1.sh ./threads 16 a
-./l1.sh ./threads 64 a
+./l1.sh ./threads 128 a
 
 ./l1.sh ./threads 16 s
-./l1.sh ./threads 64 s
+./l1.sh ./threads 128 s
 
 ./l1.sh ./threads 16 r
-./l1.sh ./threads 64 r
+./l1.sh ./threads 128 r

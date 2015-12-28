4466ed2986302984e8fc36ec4c8d4e5bd47a994e
Maciej Czekaj <mjc@semihalf.com>
2015-12-28 12:02:12 +0100

    hplot

--- hplot.sh
+++ hplot.sh
@@ -1,3 +1,4 @@
-for i in $(seq 1 100); do taskset 2 ./mem 16384 128; echo; done > out;
-cut -d\  -f2 out | ./statistics.py b c
+for i in $(seq 1 10); do taskset 2 ./mem $((1024*1024)) 128; echo; done > out
+cut -d\  -f2 out > samples.txt
+cat samples.txt | ./statistics.py b c
 gnuplot -e "plot 'b' with impulse, 'c' ; pause -1 "

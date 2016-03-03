df9102c768b15908f5ba69b7e506ef0cbea71e75
Maciej Czekaj <maciej.jakub.czekaj@gmail.com>
2016-03-03 22:37:11 +0100

    tmp

--- bench.c
+++ bench.c
@@ -17,6 +17,20 @@
 
 #define CLOCK_TYPE CLOCK_MONOTONIC_RAW
 
+double student_1_30[] = {0, 12.71,
+4.303,3.182,2.776,2.571,2.447,2.365,2.306,2.262,2.228,2.201,2.179,
+2.160,2.145,2.131,2.120,2.110,2.101,2.093,2.086,2.080,2.074,2.069,
+2.064,2.060,2.056,2.052,2.048,2.045,2.042};
+
+double student_sparse[][2] = {
+{30, 2.042},
+{40, 2.021},
+{60, 2.000},
+{80, 1.990},
+{100, 1.984},
+{1000, 1.962}
+};
+
 uint64_t getclock()
 {
 	struct timespec ts;
@@ -171,6 +185,76 @@ void (*benchmark_once)(struct thrarg *, unsigned) = benchmark_once_fork;
 static inline double sqr(double x)
 {
 	return x*x;
+}
+
+static void benchmark_auto2(struct thrarg *thrarg)
+{
+	const double min_time_ns = 10000; //10us
+	const unsigned max_iters = 10000000;
+	const unsigned max_samples = 300;
+	const double t = 2.0;
+	const double error = 0.05;
+	size_t i;
+	unsigned iters;
+
+	for (iters = 1; iters < max_iters; iters *= 2) {
+		benchmark_once(thrarg, iters);
+		if (thrarg->sum > min_time_ns)
+			break;
+	}
+
+	if (iters > max_iters) {
+		thrarg->iters = 0;
+		return;
+	}
+
+	double isamples[initial_samples];
+	fprintf(stderr, "isamples=%u iters = %u ",initial_samples, iters);
+
+	double sum = 0;
+	for (i = 0; i< initial_samples; i++) {
+		benchmark_once(thrarg, iters);
+		isamples[i] = thrarg->res;
+		sum += isamples[i];
+	}
+	double avg = sum/initial_samples;
+	double var = 0.0;
+	for (i = 0; i< initial_samples; i++)
+		var += sqr(avg - isamples[i]);
+	double std_dev = sqrt(var/(initial_samples-1));
+	fprintf(stderr, "var=%f avg=%f std_dev = %f ",var, avg, std_dev);
+
+	double n_sqrt = (2*t_initial*std_dev)/(error*avg);
+	unsigned n_final = (unsigned)ceil(sqr(n_sqrt))+4;
+	fprintf(stderr, "n_final=%u ", n_final);
+
+	double *samples = (double *)calloc(n_final, sizeof(double));
+
+	int print_samples = thrarg->print_samples;
+	sum = 0.0;
+	for (i=0; i< n_final; i++) {
+		benchmark_once(thrarg, iters);
+		samples[i] = thrarg->res;
+		sum += samples[i];
+	}
+	avg = sum/n_final;
+
+	var = 0.0;
+	for (i = 0; i< n_final; i++)
+		var += sqr(avg - samples[i]);
+	std_dev = sqrt(var/(n_final-1));
+
+	thrarg->res = avg;
+	thrarg->samples = n_final;
+	thrarg->iters = iters;
+	thrarg->sum = sum;
+	thrarg->sdev = std_dev;
+
+	fprintf(stderr, "sdev = %f\n",std_dev);
+	if (print_samples)
+		for (i = 0; i< n_final; i++)
+			fprintf(stderr, "%f\n", samples[i]);
+	free(samples);
 }
 
 static void benchmark_auto(struct thrarg *thrarg)

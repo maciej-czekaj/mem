fa9db0261a494e56ae92c952a21e06dbf7899f62
Maciej Czekaj <mjc@semihalf.com>
2016-03-22 10:20:49 +0100

    Comments

--- ring.c
+++ ring.c
@@ -30,28 +30,35 @@ struct message
 
 struct ring {
 	unsigned len;
-	unsigned received __attribute__((aligned(CACHE_LINE)));
-	unsigned sent __attribute__((aligned(CACHE_LINE)));
+	unsigned received
+		__attribute__((aligned(CACHE_LINE)));
+	unsigned sent
+		__attribute__((aligned(CACHE_LINE)));
 	struct message messages[0];
-}__attribute__((aligned(CACHE_LINE))) *R;
-
-#ifndef WITH_LOCK
+}__attribute__((aligned(CACHE_LINE)));
 
 bool ring_send(struct ring *r, size_t n, struct message msg[n])
 {
-	unsigned received, sent, len, space, mask;
+	unsigned received, sent, len, free_entries, mask;
 
+	// Do sent pisze bieżący wątek, więc nie trzeba bariery.
 	sent = r->sent;
-	len = r->len;
+	len = r->len; // len jest stałe
 	mask = len - 1;
+	// Do received pisze wątek konsumenta więc
+	// potrzebna jest bariera.
 	received  = __atomic_load_n(&r->received, __ATOMIC_ACQUIRE);
-	space = len + received - sent;
-	if (space < n)
-		return false;
+	/* Oblicz wolne miejsce */
+	free_entries = len + received - sent;
+	if (free_entries < n)
+		return false; // za mało miejsca
 
+	// Kopiuj wiadomości
 	for (size_t i = 0; i < n; i++)
 		r->messages[(sent+i) & mask] = msg[i];
 
+	// Publikuj nową wartosć sent do innych rdzeni (bariera release)
+	// Gwarantuje, że wiadomości zostały uprzednio skoiowane
 	__atomic_store_n(&r->sent, sent + n, __ATOMIC_RELEASE);
 
 	return true;
@@ -60,161 +67,67 @@ bool ring_send(struct ring *r, size_t n, struct message msg[n])
 
 bool ring_receive(struct ring *r, size_t n, struct message msg[n])
 {
-	unsigned received, sent, len, space, mask;
+	unsigned received, sent, len, valid_entries, mask;
 
+	// do received pisze ten wątek, nie trzeba bariery
 	received = r->received;
-	len = r->len;
+	len = r->len; // tylko do odczytu
 	mask = len - 1;
+	// sent jest publikowaqny przez wątek producenta
+	// trzeba użyć bariery acquire
 	sent  = __atomic_load_n(&r->sent, __ATOMIC_ACQUIRE);
-	space = sent - received;
-	if (space < n)
+	// Oblicz, ile wiadomości czeka
+	valid_entries = sent - received;
+	if (valid_entries < n) // za mało
 		return false;
 
+	// Kopiuj wiadomości
 	for (size_t i = 0; i < n; i++)
 		msg[i] = r->messages[(received+i) & mask];
 
+	// Publikuj nową wartośc received do innych rdzeni (bariera release)
+	// Gwarantuje, że wiadomości zostały uprzednio skoiowane
 	__atomic_store_n(&r->received, received + n, __ATOMIC_RELEASE);
 
 	return true;
 }
-#endif
 
-void send(size_t n)
+void send(size_t n, struct ring *r)
 {
 	struct message msg = {.count = 0};
 
 	for (size_t i = 0; i < n; i++) {
 		msg.count = i;
-		while (!ring_send(R, 1, &msg))
+		while (!ring_send(r, 1, &msg))
 			;
 	}
 }
 
-void recv(size_t n)
+void recv(size_t n, struct ring *r)
 {
 	struct message msg;
 
 	for (size_t i = 0; i < n; i++) {
-		while (!ring_receive(R, 1, &msg))
+		while (!ring_receive(r, 1, &msg))
 			;
 		assert(msg.count == i);
 	}
 }
 
-//#define CAS
-#ifdef WITH_LOCK
-static void lock(unsigned *lock)
-{
-#ifdef CAS
-	unsigned lock_val = 0;
-	while(!__atomic_compare_exchange_n(
-				lock, &lock_val, 1, false,
-				__ATOMIC_ACQUIRE, __ATOMIC_RELAXED))
-		while ((lock_val = __atomic_load_n(lock, __ATOMIC_ACQUIRE)) != 0)
-			;
-#else
-	do {
-		while ((__atomic_load_n(lock, __ATOMIC_RELAXED)) != 0)
-			;
-			//sched_yield();
-	} while (__atomic_test_and_set(lock, __ATOMIC_ACQUIRE));
-#endif
-}
-
-static void unlock(unsigned *lock)
-{
-	assert_eq(*lock, 1);
-#ifdef CAS
-	__atomic_store_n(lock, 0, __ATOMIC_RELEASE);
-#else
-	__atomic_clear(lock,__ATOMIC_RELEASE);
-#endif
-}
-
-bool ring_send(struct ring *r, size_t n, struct message msg[n], bool excl)
-{
-	unsigned received, sent, len, space, mask;
-
-	if (excl)
-		lock(&r->lock);
-
-	sent = r->sent;
-	len = r->len;
-	mask = len - 1;
-	if (excl)
-		received = r->received;
-	else
-		received  = __atomic_load_n(&r->received, __ATOMIC_ACQUIRE);
-	space = sent - received + n;
-	if (space > len) {
-		if (excl)
-			unlock(&r->lock);
-		return false;
-	}
-	for (size_t i = 0; i < n; i++)
-		r->messages[(sent+i) & mask] = msg[i];
-
-	if (excl) {
-		r->sent += n;
-		unlock(&r->lock);
-	}
-	else {
-		__atomic_store_n(&r->sent, sent + n, __ATOMIC_RELEASE);
-	}
-	return true;
-}
-
-
-bool ring_receive(struct ring *r, size_t n, struct message msg[n], bool excl)
-{
-	unsigned received, sent, len, space, mask;
-
-	if (excl)
-		lock(&r->lock);
-	received = r->received;
-	len = r->len;
-	mask = len - 1;
-	if (excl)
-		sent = r->sent;
-	else
-		sent = __atomic_load_n(&r->sent, __ATOMIC_ACQUIRE);
-	space = sent - received;
-	if (space < n) {
-		if (excl)
-			unlock(&r->lock);
-		return false;
-	}
-
-	for (size_t i = 0; i < n; i++)
-		msg[i] = r->messages[(received+i) & mask];
-
-	if (excl) {
-		r->received += n;
-		unlock(&r->lock);
-	} else {
-		__atomic_store_n(&r->received, received + n, __ATOMIC_RELEASE);
-	}
-	return true;
-}
-
-#endif
-
+struct ring *R;
 
 void benchmark_ping(struct thrarg *arg)
 {
 	if (arg->params.id)
-		send(arg->params.iters);
+		send(arg->params.iters, R);
 	else
-		recv(arg->params.iters);
+		recv(arg->params.iters, R);
 }
 
 void ring_reset(struct ring *r)
 {
 	r->sent = 0;
 	r->received = 0;
-#ifdef WITH_LOCK
-	r->lock = 0;
-#endif
 }
 
 struct ring *ring_new(size_t ring_len)
